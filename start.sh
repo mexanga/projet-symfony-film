67d29b72f5921d826bf6d6baf9366f0ea6fda844
Ulysse arnaud <mexanga@gmail.com>
2020-01-16 02:44:17 +0100

    Implémentation de docker et ajout de scripts d'installation pour Linux/Mac et Windows respectivement dans les fichiers start.sh et start.bat

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 composer dumpa
-docker cp . symfony-5_web_1:/var/www/project
+docker cp . symfony-film-web:/var/www/project
 docker-compose up --build -d
-docker exec -it symfony-5_web_1 bash -c "chmod -R 777 /var/www/project/var/cache/dev && chmod -R 777 /var/www/project/var/log && chmod -R 777 /var/log"
-
-# Pour lancer le container :
-#
-# sh start.sh
-# docker exec -it symfony-5_web_1 bash -c "chmod -R 777 /var/www/project/var/cache/dev && chmod -R 777 /var/www/project/var/log && chmod -R 777 /var/log"
+docker exec -it projet-film-symfony-web bash -c "chmod -R 777 /var/www/project/var/cache/dev && chmod -R 777 /var/www/project/var/log && chmod -R 777 /var/log"
