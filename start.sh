86b8110673c1f5d24f21ba6e8f0532707de07ea3
Ulysse arnaud <mexanga@gmail.com>
2020-01-16 03:36:58 +0100

    Résolution de docker en essayant la copie de public/index au sein du container

--- start.sh
+++ start.sh
@@ -7,3 +7,4 @@ npm install
 docker cp . symfony-film-web:/var/www/project
 docker-compose up --build -d
 docker exec -it symfony-film-web bash -c "chmod -R 777 /var/www/project/var/cache/dev && chmod -R 777 /var/www/project/var/log && chmod -R 777 /var/log"
+docker cp ./public/index.php symfony-film-web:/var/www/project/public/index.php
