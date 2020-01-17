a9093f8ccb907565c3dfa1b5af0013f752768ab4
Inès <ines.legaud@outlook.com>
2020-01-17 16:09:42 +0100

    Implémentation des jeux videos : création de l'entité + CRUD

---
+++ src/Entity/Game.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $releasedDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReleasedDate(): ?\DateTimeInterface
+    {
+        return $this->releasedDate;
+    }
+
+    public function setReleasedDate(?\DateTimeInterface $releasedDate): self
+    {
+        $this->releasedDate = $releasedDate;
+
+        return $this;
+    }
+}
